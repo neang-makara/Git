89c13fd3f7b73f2f260fa8ebe9d274bed3675372
unknown <layvirak9999@gmail.com>
2020-11-06 14:50:56 +0700

    VIRAK

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final scaffold = GlobalKey<ScaffoldState>();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        key: scaffold,
+        body: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                leading: Icon(Icons.add),
+                actions: [Icon(Icons.favorite)],
+                expandedHeight: 300.0,
+                floating: false,
+                pinned: true,
+                backgroundColor: Colors.transparent,
+                flexibleSpace: FlexibleSpaceBar(
+                    centerTitle: true,
+                    title: Text(
+                      "Collapsing Toolbar",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    background: Image.asset(
+                      'Assets/Images/ShirtShop.jpg',
+                      fit: BoxFit.cover,
+                    )),
+              ),
+            ];
+          },
+          body: Center(
+            child: Text("Sample Text"),
+          ),
+        ),
+      ),
+    );
+  }
+}
